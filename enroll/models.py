2678851fffffd5201dda23dbc77a96e5dc0151cf
PranavAchrekar <64123078+PranavAchrekar@users.noreply.github.com>
2022-12-26 17:18:19 +0530

    Update models.py

--- enroll/models.py
+++ enroll/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
-# Create your models here.
+# Create your models below here
 class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=None, null=True)
     title = models.CharField(max_length=400, null=True, blank=True)
