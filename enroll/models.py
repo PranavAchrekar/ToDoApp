042891b2d155e219fb67b9805327aaf75983cb9b
PranavAchrekar <64123078+PranavAchrekar@users.noreply.github.com>
2022-12-27 10:23:53 +0530

    Update models.py

--- enroll/models.py
+++ enroll/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 # Create your models below here
 class Task(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None, null=True) # if user is deleted then all the tasks will be also deleted
     title = models.CharField(max_length=400, null=True, blank=True)
     completed = models.BooleanField(default=False, null=True, blank=True)
     created = models.DateField(default=timezone.now, null=True, blank=True)
